cc8b1ba199ddb78f5b14efac964419655bcac756
hanny26 <hannyvyas109@gmail.com>
2024-04-12 12:04:13 +0530

    db set up in app.js and index

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 const dotenv = require('dotenv');
 dotenv.config()
-
+const {app} = require('./app');
 const mongoose = require('mongoose');
 const { DB_NAME } = require("./constants");
 const connectDB = require('./db');
 
 
-connectDB();
+connectDB().then(() => {
+    app.listen(process.env.PORT|| 8000, () => {
+        console.log(`Server is running on port : ${process.env.PORT}`);
+    })
+}).catch((error) => {
+    console.log("Error connecting to DB: ", error);
+})
