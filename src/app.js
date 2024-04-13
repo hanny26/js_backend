43f12c775e724b9494e0853ea316cfa7baa76092
hanny26 <hannyvyas109@gmail.com>
2024-04-13 20:39:30 +0530

    cors origin setup

--- src/app.js
+++ src/app.js
@@ -1,6 +1,14 @@
 const express = require('express');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
 
 const app = express();
 
+app.use(cors({
+    origin : process.env.CORS_ORIGIN,
+    credentials : true
+}));
 
-module.exports = app;
+app.use(express.json({limit: "16kb"}));
+
+module.exports = {app} ;
