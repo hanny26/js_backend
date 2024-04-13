6076e73caec7c678aefc6c01fb1c07460f645687
hanny26 <hannyvyas109@gmail.com>
2024-04-14 02:08:59 +0530

    async handler , request handler

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,19 @@
+const asyncHandler = (requestHandler) => {
+     (req, res, next) => {
+        Promise.resolve(requestHandler(req,res,next)).catch((err) => next(err))
+     }
+}
+
+module.exports = {asyncHandler} ;
+
+
+// ye kar kuch nhi raha ye wrapper func bana rha hai jo sab jagah use hota hai , kahipe promisses vala bi hota hai toh usse promisses me kaise convert kre to vo kaise convet kre to upar dekhege   
+// const asyncHandler = (fn) =>  async (req,res, next) => {
+//      try {
+//          await fn(req,res,next);
+//      } catch (error) {
+//         res.status(err.code || 500).json({
+//             message : err.message || 'An unknown error occurred'
+//         });
+//      }
+// }
