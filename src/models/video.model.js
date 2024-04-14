c7dbc05b1520659d26eecd27622dd727a80ff3c6
hanny26 <hannyvyas109@gmail.com>
2024-04-15 00:27:37 +0530

    mongo db aggregate

---
+++ src/models/video.model.js
@@ -0,0 +1,48 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const videoSchema = new Schema(
+    {
+   videoFile: {
+     type: String, //string ayega cludinaru url se
+     required: true
+   },
+   thumbnail: {
+     type: String, //string ayega cludinaru url se
+     required: true
+   },
+    title: {
+      type: String,
+      required: true
+    
+    },
+    description: {
+      type: String,
+        required: true
+    },
+    duration:{
+      type: Number,
+      required: true
+    },
+
+    views: {
+      type: Number,
+      default: 0
+    },
+
+    isPublieshed: {
+      type: Boolean,
+      default: true
+    },
+   
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    }
+},
+ { timestamps: true}
+);
+
+module.exports = mongoose.model("Video", videoSchema);
+// export const Video = mongoose.model("Video", videoSchema);
+
