7ff6165ec05528327eb724637e099700195afcd1
hanny26 <hannyvyas109@gmail.com>
2024-04-15 00:31:08 +0530

    mongooseaggregate paginate

--- src/models/video.model.js
+++ src/models/video.model.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
+const mongooseAggregatePaginate = require("mongoose-aggregate-paginate-v2");
 
 const videoSchema = new Schema(
     {
