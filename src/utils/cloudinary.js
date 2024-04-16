ae948633954c2533fc88523f75e89ca46227417b
hanny26 <hannyvyas109@gmail.com>
2024-04-16 15:32:27 +0530

    cloudinary .js and env

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,9 @@
+const {v2} = require('cloudinary');
+//fs is a file system 
+const fs = require('fs');
+
+cloudinary.config({ 
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+  api_key: process.env.CLOUDINARY_API_KEY, 
+  api_secret: process.env.CLOUDINARY_API_SECRET 
+});
