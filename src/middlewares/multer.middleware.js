341a64d5d26594568d38fd951f862418bb82632e
hanny26 <hannyvyas109@gmail.com>
2024-04-17 01:03:24 +0530

    ,ulter file upload

---
+++ src/middlewares/multer.middleware.js
@@ -0,0 +1,16 @@
+const multer = require('multer');
+//cb is callback
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, './public/temp')
+    },
+    filename: function (req, file, cb) {
+      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+      cb(null, file.fieldname + '-' + uniqueSuffix)
+    }
+  })
+  
+  const upload = multer({ storage: storage })
+
+    module.exports = upload;
