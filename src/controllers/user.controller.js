ba836f4874dcf22458ae1a745f1ddb80a15d0971
hanny26 <hannyvyas109@gmail.com>
2024-04-17 22:39:47 +0530

    routes , user controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,1 +1,10 @@
 const {asyncHandler} = require('../utils/asyncHandler');
+
+const registerUser = asyncHandler(async (req, res) => {
+    response.status(200).json({
+        success: true,
+        message: 'User registered successfully'
+    });
+})
+
+module.exports = {registerUser};
