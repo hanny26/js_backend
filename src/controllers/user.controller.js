09e4e00b5ba6637154a92e474dd4ad92aabfdd33
hanny26 <hannyvyas109@gmail.com>
2024-04-19 13:28:07 +0530

    getting the user , from user model

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,4 +1,6 @@
 const {asyncHandler} = require('../utils/asyncHandler.js');
+const {ApiError} = require('../utils/ApiError.js');
+const User = require('../models/user.model.js');
 
 const registerUser = asyncHandler(async (req, res) => {
     // get user details from frontend 
@@ -10,6 +12,27 @@ const registerUser = asyncHandler(async (req, res) => {
     // remove password and refresh token field from password
     //check for user creation 
     // return res
+
+   //if form se ya json se data aa rha hai toh req.body me ayega , but url se a rha hai toh age dekhege 
+   const {fullname, email , username , password} = req.body;
+   console.log("email: ", email);
+  
+//    getting the user details
+    if(
+        [fullname, email, username, password].some((field) => field?.trim() === "")
+    ){
+        throw new ApiError(400, "All fields are required");    
+    }
+
+//checking user already exist or not
+
+
+
+
+
 })
+
+
+
 
 module.exports = {registerUser};
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -2,7 +2,21 @@
 const express = require('express');
 const router = express.Router();
 const { registerUser } = require('../controllers/user.controller.js');
+const upload = require('../middlewares/multer.middleware.js');
 
-router.post('/register', registerUser);
+router.post('/register',upload.fields([
+  {
+    name: 'avatar', 
+    maxCount: 1
+  },
+  {
+    name: 'cover', 
+    maxCount: 1
+  }
+]),
+ registerUser);
 
 module.exports = router;
+
+
+//fields ek hi field me diff array , and  array me multiple files upload krne ke liye, single file upload krne ke liye single array me daal denge
