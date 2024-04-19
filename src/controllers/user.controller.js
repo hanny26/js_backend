0ef1e14972ab36ee3954220598ca51179a28c23d
hanny26 <hannyvyas109@gmail.com>
2024-04-19 11:14:20 +0530

    for registration

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,10 +1,15 @@
 const {asyncHandler} = require('../utils/asyncHandler.js');
 
 const registerUser = asyncHandler(async (req, res) => {
-   res.status(200).json({
-        success: true,
-        message: 'User registered successfully'
-    });
+    // get user details from frontend 
+    // Validation - not empty and email format
+    // check if user already exists: username , email 
+    // check for cover images , check for Avatar 
+    //  upload them to cloudinary
+    // create user object - creat entry in db 
+    // remove password and refresh token field from password
+    //check for user creation 
+    // return res
 })
 
 module.exports = {registerUser};
