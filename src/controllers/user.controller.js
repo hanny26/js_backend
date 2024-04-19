deae938b4ec119dcabb8c04eac4618f5cc97e0b9
hanny26 <hannyvyas109@gmail.com>
2024-04-19 16:05:02 +0530

    entry in database

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,6 +1,7 @@
 const {asyncHandler} = require('../utils/asyncHandler.js');
 const {ApiError} = require('../utils/ApiError.js');
 const User = require('../models/user.model.js');
+const {  uploadOnCloudinary } = require('../utils/cloudinary.js');
 
 const registerUser = asyncHandler(async (req, res) => {
     // get user details from frontend 
@@ -24,8 +25,44 @@ const registerUser = asyncHandler(async (req, res) => {
         throw new ApiError(400, "All fields are required");    
     }
 
-//checking user already exist or not
+//checking user already exist or not, findOne() is a method which find the user 
+const existedUser= User.findOne({
+    $or: [
+        {email: email},
+        {username: username}
+    ]
+});
+// lets check for existedUser , hai to error , nahi hai toh create, 409 user ka error
 
+if(existedUser){
+    throw new ApiError(409, "User or email already exists");
+}
+
+//for avatar and cover image
+const  avatarLocalPath = req.files?.avatar[0]?.path;
+ const coverImageLocalPath = req.files?.coverImage[0]?.path;
+//checking avatar aya hai ki nhi 
+if (!avatarLocalPath) {
+    throw new ApiError(400, "Avatar is required");
+}
+
+const avatar = await uploadOnCloudinary(avatarLocalPath);
+
+const coverImage = await uploadOnCloudinary(coverImageLocalPath);
+if (!avatar) {
+    throw new ApiError(400, "avatar file is requires");
+}
+
+// now entry in database
+ const user = await User.create({
+    fullName,
+    avatar:avatar.url,
+    coverImage: coverImage?.url || "",
+    email,
+    password,
+    username: username.toLowerCase(),
+})
+// checking user empty or not 
 
 
 
